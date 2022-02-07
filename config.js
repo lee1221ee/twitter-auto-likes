d18cb3668c85b243fd521d4c8a243926b94782ab
lee1221ee <lee19941221ee@gmail.com>
2022-02-08 01:53:16 +0800

    Build a basic auto-likes bot

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+  consumer_key: "ENTER YOUR KEY HERE",
+  consumer_secret: "ENTER YOUR KEY HERE",
+  access_token_key: "ENTER YOUR KEY HERE",
+  access_token_secret: "ENTER YOUR KEY HERE",
+  user_id: "1446550953679724545",
+  users: ["elonmusk", "mikecryptolife", "yescooler7985"]
+}
+export { config as default }
---
+++ index.js
@@ -0,0 +1,39 @@
+import { TwitterApi } from "twitter-api-v2"
+import config from "./config.js"
+
+const client = new TwitterApi({
+  appKey: config.consumer_key,
+  appSecret: config.consumer_secret,
+  accessToken: config.access_token_key,
+  accessSecret: config.access_token_secret
+})
+
+async function likesTweet(tweet) {
+  await client.v2
+    .like(config.user_id, tweet.id)
+    .then(() => {
+      return
+    })
+    .catch((error) => {
+      throw new Error(
+        "Too Many Requests, likes a Tweet has a user rate limit of 50 Requests per 15-minute"
+      )
+    })
+}
+
+for (const user of config.users) {
+  let query = "from:" + user + " -is:retweet"
+  const jsTweets = await client.v2.search(query)
+  for (const tweet of jsTweets) {
+    await likesTweet(tweet)
+      .then(() => {
+        let text = tweet.text.split("\n").join(" ").trim().substring(0, 128)
+        console.log("[" + user + "] \u2764\uFE0F  " + text)
+      })
+      .catch((error) => {
+        console.log(error)
+        process.exit(0)
+      })
+  }
+  console.log("----------------------------------------")
+}
