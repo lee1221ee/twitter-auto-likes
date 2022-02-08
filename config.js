d0503722e03c8ad202f46146435871ddcd6496ff
lee1221ee <91650059+lee1221ee@users.noreply.github.com>
2022-02-08 15:45:26 +0800

    Update config.js for Config vars

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
-  consumer_key: "ENTER YOUR KEY HERE",
-  consumer_secret: "ENTER YOUR KEY HERE",
-  access_token_key: "ENTER YOUR KEY HERE",
-  access_token_secret: "ENTER YOUR KEY HERE",
+  consumer_key: process.env.consumer_key || "ENTER YOUR KEY HERE",
+  consumer_secret: process.env.consumer_secret || "ENTER YOUR KEY HERE",
+  access_token_key: process.env.access_token_key || "ENTER YOUR KEY HERE",
+  access_token_secret: process.env.access_token_secret || "ENTER YOUR KEY HERE",
   user_id: "1446550953679724545",
   users: ["elonmusk", "mikecryptolife", "yescooler7985"]
 }
